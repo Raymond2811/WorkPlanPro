124219f69cae744643b4e193406f0f3aa6957f55
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-09-03 18:56:40 -0700

    add real time block color coding

--- script.js
+++ script.js
@@ -4,6 +4,25 @@
 $(document).ready(function (){
   const currentDate = dayjs().format('dddd, MMMM D, YYYY');
   $('#currentDay').text(currentDate);
+
+  function updateBlockColors() {
+    const currentHour = dayjs().hour();
+
+    $('.time-block').each (function() {
+      const blockHour = parseInt ($ (this). attr ('id').split('-')[1]);
+      
+      if (blockHour < currentHour){
+        $(this).addClass('past').removeClass('present future');
+      } else if (blockHour === currentHour ){
+        $(this).addClass('present').removeClass('past future');
+      } else{
+        $(this).addClass('future').removeClass('past present');
+      }
+    });
+  }
+
+  updateBlockColors();
+  setInterval(updateBlockColors, 60000);
 });
 
 
